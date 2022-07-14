c0f1034e5211368fab5a21c36641d4b0f4a37ce0
laban405 <laban405@gmail.com>
2022-07-14 21:31:44 +0300

    scale transition style

--- src/utils/page-transitions.ts
+++ src/utils/page-transitions.ts
@@ -1,25 +1,25 @@
 export const pageVariants = {
     initial: {
       opacity: 0,
-      x: "-50vw",
-      scale: 0.8
+      // x: "-50vw",
+      scale: 1.2
     },
     in: {
       opacity: 1,
-      x: 0,
+      // x: 0,
       scale: 1
     },
     out: {
-      opacity: 0,
-      x: "100vw",
-      scale: 0.8
+      // opacity: 0,
+      // x: "100vw",
+      // scale: 1.2
     }
   };
 
   export const pageTransition = {
     type: "tween",
     ease: "linear",
-    duration: 0.3
+    duration: 0.2
   };
   
   
